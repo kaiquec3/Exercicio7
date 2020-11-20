9fa5e77660f77adedcdd3054d40a8633d9358197
kaiquec3 <74798358+kaiquec3@users.noreply.github.com>
2020-11-20 20:17:16 -0300

    Add files via upload

---
+++ Exercício 7/Exercício 7/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercício_7
+{
+    class Dolar
+    {
+        double valor;
+        double convertido;
+
+        public void SetValor(double p)
+        {
+            valor = p;
+        }
+
+        public double GetValor()
+        {
+            return valor;
+        }
+
+        public double GetConvertido()
+        {
+            return convertido;
+        }
+
+        public double ConverterDolarParaReal()
+        {
+            convertido = valor * 5.46;
+            return convertido;
+        }
+    }
+}
---
+++ Exercício 7/Exercício 7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Exercício_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a1 = new Dolar();
+            double k;
+
+            do
+            {
+                Console.Write("Informe o valor em dolar: ");
+                k = double.Parse(Console.ReadLine());
+                if (k >= 0) a1.SetValor(k);
+            }
+            while (k < 0);
+
+
+            if (a1.GetValor() == 1) Console.WriteLine($"{a1.GetValor()} dolar é igual a {a1.ConverterDolarParaReal()} reais");
+            else if (a1.ConverterDolarParaReal() == 1)
+            {
+                Console.WriteLine($"{a1.GetValor()} dolares é igual a {a1.ConverterDolarParaReal()} real");
+            }
+            else Console.WriteLine($"{a1.GetValor()} dolares é igual a {a1.ConverterDolarParaReal()} reais");
+        }
+    }
+}
